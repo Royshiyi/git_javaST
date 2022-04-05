17bce62d1b613f0d419030f89bd286890e1552c6
Royshiyi <2726947965@qq.com>
2022-04-05 22:14:06 +0800

    4.05

---
+++ day20220405/TestString.java
@@ -0,0 +1,18 @@
+package day20220405;
+
+public class TestString {
+
+    void method1(){
+        String s1 = "hello";
+        String s2 = new String("world");
+        
+        char[] arr = {'a','b','c'};
+        String s3 = new String(arr);
+
+        String s4 = new String();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
